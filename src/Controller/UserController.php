b0448971fa59a4db5e35fedf11e25a0e3741cb37
HOARAU Mathieu <Mathieuhoarau@laposte.net>
2018-11-27 14:11:44 +0100

    PSR

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\UserType;
-use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -37,5 +36,4 @@ class UserController extends AbstractController
             'form' => $form->createView(),
         ]);
     }
-
 }
