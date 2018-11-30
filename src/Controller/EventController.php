28d540a648984b897310fca0a9e49ab207c0730b
milonte <milonte@gmaim.com>
2018-11-30 11:45:51 +0100

    fix use manquant

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\EventRepository;
+use App\Entity\Timer;
 
 class EventController extends AbstractController
 {
