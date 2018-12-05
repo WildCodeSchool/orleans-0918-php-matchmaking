aa016c6599996d3cd26ffc88896942e6fdfd4717
Eric Rousselet <rousselet.eric@gmail.com>
2018-12-05 11:59:40 +0100

    javascript terminé

--- assets/js/updateForm.js
+++ assets/js/updateForm.js
@@ -1,11 +1,36 @@
 
 let userFormElt = document.getElementById('userForm');
+let addButton = document.getElementById('addButton');
+let editButton = document.getElementById('editButton');
 
 userFormElt.style.display = "none";
 
     $('.createButton').click(function() {
+
+        document.getElementById("user_firstName").value = "";
+        document.getElementById("user_lastName").value = "";
+        document.getElementById("user_email").value = "";
+
+        if(addButton.style.display === "none")
+            editButton.style.display = "none";
+            addButton.style.display = "block";
         if(userFormElt.style.display === "none")
              userFormElt.style.display = "block";
-        else
-            userFormElt.style.display = "none";
+    });
+
+    $('.listButton').click(function() {
+        let td=this.parentNode.parentNode;
+
+        let firstName=td.children[0].innerText;
+        let lastName=td.children[1].innerText;
+        let email=td.children[2].innerText;
+
+        document.getElementById("user_firstName").value = firstName;
+        document.getElementById("user_lastName").value = lastName;
+        document.getElementById("user_email").value = email;
+
+            addButton.style.display = "none";
+            editButton.style.display = "block";
+        if(userFormElt.style.display === "none")
+            userFormElt.style.display = "block";
     });
