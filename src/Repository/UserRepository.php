dbe5dd08fc3756cd4d2552d4572966add0ec22ec
Eric Rousselet <rousselet.eric@gmail.com>
2018-12-17 09:47:01 +0100

    tri des utilisateurs pas nom

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -9,7 +9,6 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
 /**
  * @method User|null find($id, $lockMode = null, $lockVersion = null)
  * @method User|null findOneBy(array $criteria, array $orderBy = null)
- * @method User[]    findAll()
  * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class UserRepository extends ServiceEntityRepository
@@ -17,5 +16,10 @@ class UserRepository extends ServiceEntityRepository
     public function __construct(RegistryInterface $registry)
     {
         parent::__construct($registry, User::class);
+    }
+
+    public function findAll()
+    {
+        return $this->findBy(array(), array('lastName' => 'ASC'));
     }
 }
