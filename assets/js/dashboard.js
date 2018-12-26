c26bf95e96fc4ef707d53253caa7ba44d14cc158
milonte <milonte@gmaim.com>
2018-12-26 10:03:37 +0100

    add timer js

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,1 +1,16 @@
 require('../css/dashboard.scss');
+
+var $ = require('jquery');
+require('bootstrap');
+
+/* let timerElt = document.getElementById("roundTimer").children[0];
+timerElt.textContent = "oui"; */
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    let timerElt = document.getElementById("roundTimer");
+    var minutes = timerElt.dataset.roundMinutes;
+    var seconds = timerElt.dataset.roundSeconds;
+    timerElt.children[0].textContent = minutes + " : " + seconds;
+});
