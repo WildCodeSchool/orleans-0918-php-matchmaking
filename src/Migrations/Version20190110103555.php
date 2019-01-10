a48202ee83fbff3a30677a317b49c408701894c0
HOARAU Mathieu <Mathieuhoarau@laposte.net>
2019-01-10 12:13:15 +0100

    fix email + insertion default

--- src/Migrations/Version20190110103555.php
+++ src/Migrations/Version20190110103555.php
@@ -16,6 +16,10 @@ final class Version20190110103555 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE player CHANGE mail mail VARCHAR(255) DEFAULT NULL');
+        $this->addSql("INSERT INTO `status_event` VALUES (1,'En préparation',0,'secondary'),(2,'Inscription',1,'info'),(3,'Complet',2,'danger'),(4,'En cours',3,'success'),(5,'Terminé',4,'light')");
+        $this->addSql("INSERT INTO `timer` VALUES (1,1,0,5,0)");
+        $this->addSql("INSERT INTO `society` VALUES (1, 'PROJET I/O')");
+        $this->addSql('INSERT INTO `user` VALUES (1, 1, "matchmaking.wcs@gmail.com", "Administrateur", "Matchmaking", 1, "[\"ROLE_ADMIN\"]", "$argon2i$v=19$m=1024,t=2,p=2$YWFVY0JNamE5M3JiTjZKeQ$zMsQljfw90tiANsUKEUSHO/ZSaHyjG4xBlTx30HiTow")');
     }
 
     public function down(Schema $schema) : void
