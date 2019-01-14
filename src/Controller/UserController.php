365d6d46f7a0d1a8dd1bc6dad2d6d151b740a5df
Eric Rousselet <rousselet.eric@gmail.com>
2019-01-14 15:44:02 +0100

    unused routes deleted

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -172,34 +172,6 @@ class UserController extends AbstractController
     }
 
     /**
-     * @Route("/{id}", name="user_show", methods="GET")
-     */
-    public function show(User $user): Response
-    {
-        return $this->render('user/show.html.twig', ['user' => $user]);
-    }
-
-    /**
-     * @Route("/{id}/edit", name="user_edit", methods="GET|POST")
-     */
-    public function edit(Request $request, User $user): Response
-    {
-        $form = $this->createForm(User1Type::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('user_index', ['id' => $user->getId()]);
-        }
-
-        return $this->render('user/edit.html.twig', [
-            'user' => $user,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
      * @Route("/{id}", name="user_delete", methods="DELETE")
      * @param Request $request
      * @param User $user
